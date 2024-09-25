bc73bec54b34c8f7a72a6015b7652b2c8ee29d70
Daulton Truett <dtruett45@gmail.com>
2024-09-25 06:07:26 -0500

    Updated navbar with Angular-Material toolbar and responsive sidenav. Navbar is now implemented in app.component in instead of its own component.

--- app_admin/src/app/app.component.ts
+++ app_admin/src/app/app.component.ts
@@ -1,21 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDivider } from '@angular/material/divider';
+import { MatList, MatListItem } from '@angular/material/list'
+
+import { AuthenticationService } from './services/authentication.service';
 import { ListMedicationsComponent } from './list-medications/list-medications.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent,
-    ListMedicationsComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterModule,
+    ListMedicationsComponent, FooterComponent,
+    MatSidenavModule, MatIconModule, MatButtonModule, MatToolbarModule,
+    MatDivider, MatListItem, MatList
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'parastudy';
+
+  constructor(
+    private authenticationService: AuthenticationService
+    ){}
+  
+  ngOnInit(){}
+
+  public isLoggedIn(): boolean{
+    return this.authenticationService.isLoggedIn();
+  };
+
+  public onLogout(): void{
+    return this.authenticationService.logout();
+  };
+
 }
